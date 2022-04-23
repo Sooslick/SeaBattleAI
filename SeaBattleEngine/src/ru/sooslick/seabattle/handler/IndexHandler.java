c1f9a6ddac8210f949c90d66fb90a8b52244f8ea
Sooslick <lexapyc@mail.ru>
2022-04-23 13:40:48 +0300

    package setup

--- SeaBattleEngine/src/ru/sooslick/seabattle/handler/IndexHandler.java
+++ SeaBattleEngine/src/ru/sooslick/seabattle/handler/IndexHandler.java
@@ -5,21 +5,30 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import ru.sooslick.seabattle.Log;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class IndexHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String reqPath = httpExchange.getRequestURI().getPath();
-        String uri = reqPath.substring(1);
-        if ("".equals(uri))
-            uri = "index.html";
-        if (! new File(uri).exists())
-            uri = "index.html";
-        Log.info("Request web resource " + uri + " <- " + reqPath);
-        byte[] answer = ByteStreams.toByteArray(new FileInputStream(uri));
+        if ("/".equals(reqPath))
+            reqPath = "/index.html";
+        Log.info("Request web resource " + reqPath);
+        InputStream is = getClass().getResourceAsStream(reqPath);
+        if (is == null) {
+            try {
+                is = new FileInputStream(reqPath.substring(1));
+            } catch (IOException e) {
+                Log.warn("Can't load resource " + reqPath);
+                httpExchange.sendResponseHeaders(404, 3);
+                httpExchange.getResponseBody().write("404".getBytes());
+                httpExchange.close();
+                return;
+            }
+        }
+        byte[] answer = ByteStreams.toByteArray(is);
         httpExchange.sendResponseHeaders(200, answer.length);
         httpExchange.getResponseBody().write(answer);
         httpExchange.close();
